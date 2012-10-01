cec8fd330af1568f34cb35968e97af1c3c23f6db
Robert Hetzler <xore@xore.ca>
2012-09-30 23:14:04 -0700

    Vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Savannah::Application.config.secret_token = '40cf64a4ef82488b6234a55dd6d0b8aab62d581afff8dc78a7cc34fe8334a6c053c7aa3067e95ef999f95705ab11db21945918e940a4f631b2e3fbc6e33c35a6'
